43a87afe5781ff44bb99631662484f380e93ad1d
sergiop45 <sergiopsantos33@gmail.com>
2022-09-03 11:56:35 -0300

    listar-usuarios

--- crud-usuarios/listar-usuarios.php
+++ crud-usuarios/listar-usuarios.php
@@ -2,4 +2,40 @@
 
 echo "LISTAGEM DE USUARIOS";
 
+$sql = "SELECT * FROM usuarios;";
+
+$res = $conn -> query($sql);
+
+$qtd = $res->num_rows;
+echo "<br><br>  ";
+if($qtd > 0 ) {
+    echo "<table class='table table-hover table-striped table-bordered'>";
+    echo "<tr>";
+    echo "<th>Id</th>";
+    echo "<th>Nome</th>";
+    echo "<th>Email</th>";
+    echo "<th>Data de Nascimento</th>";
+    echo "<th>Ações</th>";
+    echo "</tr>";
+    while($row = $res -> fetch_object()){
+
+        echo "<tr>";
+        echo "<td>".$row->id."</td>";
+        echo "<td>".$row->nome."</td>";
+        echo "<td>".$row->email."</td>";
+        echo "<td>".$row->data_nasc."</td>";
+        echo "<td>
+            <button class='btn btn-success'>Editar</button>
+            <button class='btn btn-danger'>Deletar</button>
+        </td>";
+        echo "</tr>";
+
+    }
+    
+    echo "</table>";
+
+} else {
+    print "<p class='alert alert-danger'>Não foram encontrados usuarios!</p>";
+}
+
 ?>
--- crud-usuarios/salvar-usuario.php
+++ crud-usuarios/salvar-usuario.php
@@ -10,9 +10,18 @@
             
             $sql = "INSERT INTO usuarios (nome, email, senha, data_nasc)
              VALUES ('{$nome}', '{$email}', '{$senha}', '{$data_nasc}');";
-             echo $sql;
+            
 
             $res = $conn -> query($sql);
+
+            if($res == true) {
+                print "<script>alert('Usuario Cadastrado com sucesso!');</script>";
+                print "<script>location.href='?page=listar';</script>"; 
+            } else {
+                print "<script>alert('Erro ao cadastrar usuario!');</script>";
+                print "<script>location.href='?page=novo';</script>";
+            }
+
         break;    
 
     }
